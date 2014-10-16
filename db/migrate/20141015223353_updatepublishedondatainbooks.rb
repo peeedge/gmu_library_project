63684797170371e41af252a3c4bb7c3bd49af560
Sean Brown <seanmbrown21@gmail.com>
2014-10-16 19:10:01 -0400

    recreated user model

--- db/migrate/20141015223353_updatepublishedondatainbooks.rb
+++ db/migrate/20141015223353_updatepublishedondatainbooks.rb
@@ -1,6 +1,6 @@
 class Updatepublishedondatainbooks < ActiveRecord::Migration
   def change
   	self.up
-  	rename_column :books, :publisehd_on, :published_on
+  	rename_column :books, :published_on, :published_on
   end
 end
