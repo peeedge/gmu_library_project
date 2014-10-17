68c44653ba08f767f63e041c42e5b3a072f305f2
peeedge <pj.harms@gmail.com>
2014-10-17 19:35:48 -0400

    Checking in

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
 	@available_at = Time.now
 	@books = Book.all
 	GENRES = 'test1', 'test2', 'test3'
-	validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true
+	validates :isbn, :title, :abstract, :pages, :genre, :total_in_library, :author, presence: true
 	validates :abstract, length: { minimum: 15 }, unless: 'abstract.blank?'
 	validates :pages,
    		numericality: { only_integer: true, greater_than_or_equal_to: 0 },
