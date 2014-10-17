c32908172df6a2279dd5b8a2c35cbbd068201638
peeedge <pj.harms@gmail.com>
2014-10-17 19:16:57 -0400

    Added User Model and Database Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015223353) do
+ActiveRecord::Schema.define(version: 20141017231429) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20141015223353) do
     t.date     "published_on"
     t.integer  "total_in_library"
     t.text     "abstract"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "user_id"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
