cb5dea0178b3df5aa04ee486ad9836e8ae7775f5
peeedge <pj.harms@gmail.com>
2014-10-18 23:48:41 -0400

    Completing My Reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,23 +2,27 @@ class ReservationsController < ApplicationController
 
    def index
     @available_at = Time.now
-    @reservations = Reservation.all
+    @reservations = Reservation.where('user_id' => current_user.id)
+    #@reservations = Reservation.all
    end
 
     def show
-      @reservations = Reservation.joins(:user).where('book_id' => params[:book_id]).select("users.*,reservations.*")
-      @bookid = params[:book_id]
-
-      respond_to do |format|
-        format.html
-        format.json { render :json => @reservations }
-      end
+    #@reservations = Reservation.where('user_id' => current_user.id)
+    #@reservation = Reservation.find_by_id(params[:id]) 
+    #@book = Book.find_by_id(@reservation.book_id) 
+    #@user = User.find_by_id(@reservation.user_id) 
+    #  respond_to do |format|
+       # format.html
+      #  format.json { render :json => @reservations }
+     # end
     end
 
 
   	def create
       @reservation = Reservation.new(reservation_params)
       @reservation.user_id = current_user.id
+      @reservation.reserved_on = Date.today
+      @reservation.due_on  = Date.today + 7
 
       respond_to do |format|
         if @reservation.save
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ActiveRecord::Base
+
 	belongs_to :user #=> "User", :foreign_key => 'user_id'
 	belongs_to :book #=> "Book", :foreign_key => 'book_id'
 end
